6f662e6a4ff0cfccdeeb4e0f6eed7b320a4c6a3f
Ception <ception@live.ca>
2023-11-14 23:49:00 -0500

    tweaked a couple style things

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,8 +4,8 @@ export const ForwardArrow = component$(() => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="48"
-      height="48"
+      width="64"
+      height="64"
       viewBox="0 0 24 24"
     >
       <path
