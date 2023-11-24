0e5679d8a3873c87233cc9548c8421b67512868a
Ception <ception@live.ca>
2023-11-24 12:52:56 -0500

    removed gsap, fixed issues with changing text

--- app/_components/ChangingText.tsx
+++ app/_components/ChangingText.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 interface ChangingTextProps {
   text: string;
@@ -13,8 +13,8 @@ export const ChangingText = ({ text }: ChangingTextProps) => {
   const [title, setTitle] = useState(new Array(FINAL_TEXT.length).fill(""));
 
   useEffect(() => {
-    const indexesToShuffle = Array.from(Array(FINAL_TEXT.length).keys());
     let intervalId: NodeJS.Timeout;
+    let timeouts: NodeJS.Timeout[] = [];
 
     const shuffleCharacter = (index: number) => {
       setTitle((prevTitle) => {
@@ -26,28 +26,50 @@ export const ChangingText = ({ text }: ChangingTextProps) => {
       });
     };
 
-    intervalId = setInterval(() => {
-      if (indexesToShuffle.length > 0) {
-        const index = indexesToShuffle.shift();
-        if (index !== undefined) {
-          shuffleCharacter(index);
+    const startAnimation = () => {
+      const indexesToShuffle = Array.from(Array(FINAL_TEXT.length).keys());
+
+      intervalId = setInterval(() => {
+        if (indexesToShuffle.length > 0) {
+          const index = indexesToShuffle.shift();
+          if (index !== undefined) {
+            shuffleCharacter(index);
+          }
+        } else {
+          clearInterval(intervalId);
         }
-      } else {
+      }, 60);
+
+      indexesToShuffle.forEach((index, i) => {
+        const timeout = setTimeout(() => {
+          setTitle((prevTitle) => {
+            const newTitle = [...prevTitle];
+            newTitle[index] = FINAL_TEXT[index];
+            return newTitle;
+          });
+        }, i * 100 + 1000);
+        timeouts.push(timeout);
+      });
+    };
+
+    startAnimation();
+
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === "visible") {
         clearInterval(intervalId);
+        timeouts.forEach(clearTimeout);
+        setTitle(new Array(FINAL_TEXT.length).fill(""));
+        startAnimation();
       }
-    }, 60);
+    };
 
-    indexesToShuffle.forEach((index, i) => {
-      setTimeout(() => {
-        setTitle((prevTitle) => {
-          const newTitle = [...prevTitle];
-          newTitle[index] = FINAL_TEXT[index].toUpperCase();
-          return newTitle;
-        });
-      }, i * 100 + 1000);
-    });
+    document.addEventListener("visibilitychange", handleVisibilityChange);
 
-    return () => clearInterval(intervalId);
+    return () => {
+      clearInterval(intervalId);
+      timeouts.forEach(clearTimeout);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
   }, [FINAL_TEXT, characters]);
 
   return <span>{title.join("")}</span>;
@@ -61,12 +83,11 @@ export const ScramblePhrases = ({ text }: ScramblePhrasesProps) => {
   const FINAL_TEXT = text;
   const characters = "01";
   const [title, setTitle] = useState("");
+  const wordIndexRef = useRef(0);
+  const intervalsRef = useRef<number[]>([]);
+  const revealTimeoutsRef = useRef<number[]>([]);
 
   useEffect(() => {
-    let wordIndex = 0;
-    const intervals: number[] = [];
-    const revealTimeouts: number[] = [];
-
     const shuffleCharacter = (index: number, word: string) => {
       setTitle((prevTitle) => {
         const newTitle =
@@ -89,37 +110,39 @@ export const ScramblePhrases = ({ text }: ScramblePhrasesProps) => {
 
     const shuffleWord = (word: string) => {
       setTitle(new Array(word.length + 1).join(" "));
+      intervalsRef.current = [];
+      revealTimeoutsRef.current = [];
       for (let i = 0; i < word.length; i++) {
         startShuffling(i, word);
       }
     };
 
     const startShuffling = (index: number, word: string) => {
-      intervals[index] = window.setInterval(() => {
+      intervalsRef.current[index] = window.setInterval(() => {
         shuffleCharacter(index, word);
       }, 100);
 
-      revealTimeouts[index] = window.setTimeout(() => {
+      revealTimeoutsRef.current[index] = window.setTimeout(() => {
         stopShuffling(index, word);
       }, 1500 + index * 100);
     };
 
     const stopShuffling = (index: number, word: string) => {
-      clearInterval(intervals[index]);
+      clearInterval(intervalsRef.current[index]);
       revealCharacter(index, word);
       if (index === word.length - 1) {
         setTimeout(() => {
-          wordIndex = (wordIndex + 1) % FINAL_TEXT.length;
-          shuffleWord(FINAL_TEXT[wordIndex]);
+          wordIndexRef.current = (wordIndexRef.current + 1) % FINAL_TEXT.length;
+          shuffleWord(FINAL_TEXT[wordIndexRef.current]);
         }, 2500);
       }
     };
 
-    shuffleWord(FINAL_TEXT[wordIndex]);
+    shuffleWord(FINAL_TEXT[wordIndexRef.current]);
 
     return () => {
-      intervals.forEach((interval) => clearInterval(interval));
-      revealTimeouts.forEach((timeout) => clearTimeout(timeout));
+      intervalsRef.current.forEach((interval) => clearInterval(interval));
+      revealTimeoutsRef.current.forEach((timeout) => clearTimeout(timeout));
     };
   }, [FINAL_TEXT]);
 
