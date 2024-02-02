0ec39bc7b6a1ded6a770fd96f805ef4cc75e5292
Ception <ception@live.ca>
2024-02-02 12:05:14 -0500

    Refactor page.tsx to improve readability and remove unnecessary styles

--- app/page.tsx
+++ app/page.tsx
@@ -29,19 +29,19 @@ export default function Home() {
       <div className="h-screen w-full flex justify-center items-center relative">
         <div className="h-1/2 w-1/2 flex flex-col justify-center items-center p-12 md:p-3">
           <div className="self-start h-7 pl-12">
-            <h1 className="text-xl md:text-lg text-cyan-500">
+            <h1 className="text-xl text-cyan-500">
               <ChangingText text={MY_NAME} />
             </h1>
           </div>
           <div className="self-start pl-12">
-            <h2 className="text-8xl md:text-6xl">{PREFIX}</h2>
+            <h2 className="text-8xl">{PREFIX}</h2>
           </div>
           <div className="line"></div>
-          <div className="flex self-end w-5/6 h-24">
-            <div className="pt-4 md:pt-1">
-              <FORWARD_ARROW />
+          <div className="flex items-center self-end w-5/6 h-24">
+            <div className="h-12 w-12">
+              <FORWARD_ARROW width={48} height={48} />
             </div>
-            <h2 className="text-8xl md:text-6xl">
+            <h2 className="text-8xl">
               <ScramblePhrases text={SUFFIX_PHRASES} />
             </h2>
           </div>
