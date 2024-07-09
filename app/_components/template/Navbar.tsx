c2eb6dabf2c12fc06e74646c7f171b95c7b7c8ca
Ception <ception@live.ca>
2024-07-09 14:14:01 -0400

    - Redesigned the Header and Navbar, as well as tweaked it for better mobile responsiveness

--- app/_components/template/Navbar.tsx
+++ app/_components/template/Navbar.tsx
@@ -3,11 +3,14 @@
 import { useState, useEffect, useMemo } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 export default function Navbar({
   setIsNavOpen,
+  isMobile = false,
 }: {
   setIsNavOpen?: (isOpen: boolean) => void;
+  isMobile?: boolean;
 }) {
   const [activeItem, setActiveItem] = useState("");
   const menuItems = useMemo(
@@ -30,7 +33,7 @@ export default function Navbar({
     } else if (item === "Who Am I?") {
       return "/#about-me";
     } else {
-      return `/${item.toLocaleLowerCase()}`;
+      return `/${item.toLowerCase()}`;
     }
   };
 
@@ -41,29 +44,60 @@ export default function Navbar({
     }
   };
 
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+
   return (
-    <div className="md:ml-[-40px] z-30 fixed top-1/2 transform -translate-y-1/2 h-[calc(100vh_-_74px)] md:h-[calc(100%_-_74px)] w-full sm:w-auto">
-      <div className="flex h-full justify-center items-center w-full sm:w-auto">
-        <ul className="flex flex-col space-y-2">
-          {menuItems.map((item) => (
-            <li
-              className={`py-2 text-5xl md:text-lg leading-tight font-light text-center ${
+    <nav
+      className={`${
+        isMobile
+          ? "w-full"
+          : "md:ml-[-40px] z-30 fixed top-1/2 transform -translate-y-1/2 h-[calc(100vh_-_74px)] md:h-[calc(100%_-_74px)] w-full sm:w-auto"
+      }`}
+    >
+      <motion.ul
+        className={`flex ${
+          isMobile
+            ? "flex-col items-center space-y-6"
+            : "flex-col space-y-2 h-full justify-center"
+        }`}
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        {menuItems.map((item) => (
+          <motion.li
+            variants={itemVariants}
+            className={`py-2 ${
+              isMobile ? "text-4xl" : "text-5xl md:text-lg"
+            } leading-tight font-light text-center`}
+            key={item}
+            onMouseOver={() => setActiveItem(item)}
+          >
+            <Link
+              href={getHref(item)}
+              onClick={() => handleLinkClick(item)}
+              className={`block ${
                 activeItem === item ? "text-sky-500" : "text-white"
-              }`}
-              key={item}
-              onMouseOver={() => setActiveItem(item)}
+              } ${!isMobile ? "vertical-links-desktop" : ""}`}
             >
-              <Link
-                href={getHref(item)}
-                onClick={() => handleLinkClick(item)}
-                className="block vertical-links-desktop"
-              >
-                {item}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+              {item}
+            </Link>
+          </motion.li>
+        ))}
+      </motion.ul>
+    </nav>
   );
 }
